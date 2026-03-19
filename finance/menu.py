e511469512d17c2f0db52a51edc62bdfe0553ea7
dachrisch <christian.daehn@gmail.com>
2026-03-20 00:47:52 +0100

    feat: restrict finance menu to bumbleflies.de admins

--- finance/menu.py
+++ finance/menu.py
@@ -1,13 +1,15 @@
 from league_manager.base_menu import BaseMenu, MenuItem
+from league_manager.utils.view_utils import is_finance_admin
+
 
 class FinanceMenu(BaseMenu):
     def get_name(self):
         return "Finance"
 
     def get_menu_items(self, request):
-        if not request.user.is_staff:
+        if not is_finance_admin(request.user):
             return []
-            
+
         return [
             MenuItem.create("Dashboard", "finance-dashboard"),
         ]
