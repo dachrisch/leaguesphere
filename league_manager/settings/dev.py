0a03df804c6716420f9a2ed6c4ae9f53d273e00e
dachrisch <christian.daehn@gmail.com>
2026-04-04 21:55:36 +0200

    fix(settings): update dev settings and synchronize package-lock versions

--- league_manager/settings/dev.py
+++ league_manager/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = True
 DEBUG_DATE = datetime.date.today()
 # DEBUG_DATE = datetime.date(2026, 3, 21)
 
-DEBUG_TOOLBAR = "pytest" not in sys.modules
+DEBUG_TOOLBAR = True
 # DEBUG_TOOLBAR = False
 # PROFILING = True
 PROFILING = False
