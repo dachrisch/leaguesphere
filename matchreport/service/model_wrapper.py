8c43ed5f7bdbba52c9d910a02b087c664d662f20
Leon Friedmann <leon@friedmann.eu>
2026-05-14 12:37:14 +0200

    Fixed sorting

--- matchreport/service/model_wrapper.py
+++ matchreport/service/model_wrapper.py
@@ -173,7 +173,7 @@ class MachtreportModelWrapper:
 
         latest_license = OfficialLicenseHistory.objects.filter(
             official_id=OuterRef("official")
-        ).order_by("created_at__year", "license__name") \
+        ).order_by("-created_at__year", "license__name") \
             .values('license__name')[:1]
 
         position_order = {
